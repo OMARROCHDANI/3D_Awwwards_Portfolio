019bb558834b7f6cd147ae2477baa3945a108327
OMARROCHDANI <omarrochdani32@gmail.com>
2026-06-28 20:04:59 +0200

    added hero section with animated text, layered visuals, and smooth entrance transition

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
