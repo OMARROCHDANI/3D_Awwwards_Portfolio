0fc13e709c9a3dd4ad93b1895f1a078b898e5b1d
OMARROCHDANI <omarrochdani32@gmail.com>
2026-06-29 19:59:00 +0200

    add animated services section with GSAP and ScrollTrigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import ServiceSummary from "./sections/ServiceSummary";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <ServiceSummary />
+      <section className="min-h-screen"></section>
+      <section className="min-h-screen"></section>
+
+      <section className="min-h-screen"></section>
+
+      <section className="min-h-screen"></section>
+      <section className="min-h-screen"></section>
+
+      <section className="min-h-screen"></section>
+
+      <section className="min-h-screen"></section>
+
+      <section className="min-h-screen"></section>
+
+      <section className="min-h-screen"></section>
     </div>
   );
 };
